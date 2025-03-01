e3db01956bd5a9c0d7fc2d1283e33b52285ea970
AnHyepin <gpqls9524@naver.com>
2025-03-01 21:35:00 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/sangdaeng001sqlbank/SangDaeng001SqlBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.sangdaeng001sqlbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangDaeng001SqlBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SangDaeng001SqlBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sangdaeng001sqlbank/SangDaeng001SqlBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sangdaeng001sqlbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangDaeng001SqlBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
